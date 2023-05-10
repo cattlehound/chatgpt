11a52ff3e08ba7a57fa13c3c42000c48b34cf6f6
cattlehound <cattlehound@yahoo.com>
2023-05-10 14:15:00 +1200

    Update app.py

--- simple-flask-app-hosted-on-google-cloud-run/app.py
+++ simple-flask-app-hosted-on-google-cloud-run/app.py
@@ -1,1 +1,15 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Home Page, my guy!'
+
+@app.route('/second-route')
+def second_route():
+    return 'This is the second route.'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
